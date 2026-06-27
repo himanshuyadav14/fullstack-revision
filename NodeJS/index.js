2fd0760965d33b047caa18a32850faacf05e81cd
himanshuyadav14 <himanshu1156044@gmail.com>
2026-06-27 13:45:46 +0530

    HTTP method in NodejS

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -2,11 +2,10 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const server = http.createServer((req, res) => {
+function myHandler() {
   if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.url} Request Received\n`;
+  const log = `${Date.now()}: ${req.method} ${req.url} Request Received\n`;
   const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
   fs.appendFile("./log.txt", log, (err) => {
     if (err) {
       console.error("Failed to write log:", err);
@@ -25,12 +24,20 @@ const server = http.createServer((req, res) => {
         const search = myUrl.query.search_query;
         res.end(`Here is your search for ${search}`);
         break;
+      case "/signup":
+        if (req.method === "GET") res.end("This is a signup form");
+        else if (req.method === "POST") {
+          res.end("success");
+        }
+        break;
       default:
         res.statusCode = 404;
         res.end("404 Not found");
     }
   });
-});
+}
+
+const server = http.createServer(myHandler);
 
 server.listen(3000, () => {
   console.log("Server started on port 3000");
