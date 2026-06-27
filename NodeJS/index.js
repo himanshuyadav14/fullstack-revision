286ec6e4e77f8f321d60e753a04974b7352a3c6a
himanshuyadav14 <himanshu1156044@gmail.com>
2026-06-27 13:16:12 +0530

    Handling URL's in NodejS

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,20 +1,29 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} Request Received\n`;
+  const myUrl = url.parse(req.url, true);
+  console.log(myUrl);
   fs.appendFile("./log.txt", log, (err) => {
     if (err) {
       console.error("Failed to write log:", err);
       res.statusCode = 500;
       return res.end("Internal Server Error");
     }
-    switch (req.url) {
-      case "/about":
-        res.end("About");
-        break;
+    switch (myUrl.pathname) {
       case "/":
         res.end("Home");
+        break;
+      case "/about":
+        const username = myUrl.query.name;
+        res.end(`About ${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end(`Here is your search for ${search}`);
         break;
       default:
         res.statusCode = 404;
